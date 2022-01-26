2ff1a3330c5596149ea3cb7abcfee30193c6dfb5
petecknight <petecknight@gmail.com>
2022-01-26 11:21:12 +0000

    added deploy to local

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+mkdir /Users/pete/Applications/confluent-6.0.0/share/confluent-hub-components/file-pulse-connector
+cp connect-file-pulse-plugin/target/kafka-connect-filepulse-plugin-2.4.1-jar-with-dependencies.jar /Users/pete/Applications/confluent-6.0.0/share/confluent-hub-components/file-pulse-connector
+
+
+
